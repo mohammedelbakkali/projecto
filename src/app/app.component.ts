124589639b26da0d813584ee1e2bec4613fa3522
ZAKARIAE48CHELLE <118846093+ZAKARIAE48CHELLE@users.noreply.github.com>
2023-07-06 20:18:52 +0100

    sidebar
    side bar aded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     standalone: true,
-    imports: [NgSwitch, NgSwitchDefault, NgSwitchCase, RouterOutlet]
+    imports: [NgSwitch, NgSwitchDefault, NgSwitchCase, RouterOutlet,SideBarComponent]
 })
 export class AppComponent {
   title = 'projecto';
