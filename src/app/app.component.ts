ded64f24aa637f453727f8accba4b719a842d1fd
chihab <hellserc2@gmail.com>
2023-07-07 12:16:34 +0100

    category component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
 import { ToolbarComponent } from "./toolbar/toolbar.component";
+import { CategoryComponent } from "./category/category.component";
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     standalone: true,
-    imports: [NgSwitch, NgSwitchDefault, NgSwitchCase, RouterOutlet, MatIconModule, ToolbarComponent]
+    imports: [NgSwitch, NgSwitchDefault, NgSwitchCase, RouterOutlet, MatIconModule, ToolbarComponent, CategoryComponent]
 })
 export class AppComponent {
   title = 'projecto';
