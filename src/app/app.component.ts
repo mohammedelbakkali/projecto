52970cb98a47e5550e6c5948233c3b84b4cbbbde
chihab <hellserc2@gmail.com>
2023-07-07 11:10:37 +0100

    toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
-
+import {MatIconModule} from '@angular/material/icon';
+import { ToolbarComponent } from "./toolbar/toolbar.component";
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     standalone: true,
-    imports: [NgSwitch, NgSwitchDefault, NgSwitchCase, RouterOutlet]
+    imports: [NgSwitch, NgSwitchDefault, NgSwitchCase, RouterOutlet, MatIconModule, ToolbarComponent]
 })
 export class AppComponent {
   title = 'projecto';
