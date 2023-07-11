a33d61e1c2db91be6a49fb5c5d72126c1ee3691e
chihab <hellserc2@gmail.com>
2023-07-11 18:36:32 +0100

    finalize category

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
+import { SubCategoryComponent } from "../sub-category/sub-category.component";
+import { Category } from '../shared/models/category.model';
 @Component({
-  selector: 'app-category',
-  standalone: true,
-  imports: [CommonModule,MatIconModule],
-  templateUrl: './category.component.html',
-  styleUrls: ['./category.component.scss']
+    selector: 'app-category',
+    standalone: true,
+    templateUrl: './category.component.html',
+    styleUrls: ['./category.component.scss'],
+    imports: [CommonModule, MatIconModule, SubCategoryComponent]
 })
 export class CategoryComponent {
+   // subCategories: Category[] = [{icon: "home",label:"home" },{icon: "home",label:"home" }];
+display: Boolean =false;
+    selectedCategory: string = '';
+    subCategories: Category[]=[];
+showSubCategory(selectedCategory: string) {
+    if(this.selectedCategory == selectedCategory || this.selectedCategory == '')
+    this.display= !this.display;
+    this.selectedCategory = selectedCategory;
+    /** this will be replaced with a service or other method  */
+    switch(selectedCategory){
+        case 'Home':
+            this.subCategories = [{icon: "loan",label:"loan" },{icon: "rent",label:"rent" }];
+            break;
+        case '':
+            this.subCategories = [{icon: "home",label:"home" },{icon: "home",label:"home" }];
+            break;
+
+    }
+    /** to here */
+}
 
 }
