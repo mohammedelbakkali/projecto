4d6f9fbf6e8a1f4bf7946782f6a20c76a067b5c5
chihab <hellserc2@gmail.com>
2023-07-13 17:14:19 +0100

    create card prototype

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from "../card/card.component";
 import { MiniProjectCardComponent } from "./mini-project-card/mini-project-card.component";
-
+import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
-    imports: [CommonModule, CardComponent, MiniProjectCardComponent]
+    imports: [CommonModule, CardComponent, MiniProjectCardComponent,MatIconModule,MatButtonModule]
 })
 export class HomeComponent {
 
--- src/app/home/mini-project-card/mini-project-card.component.ts
+++ src/app/home/mini-project-card/mini-project-card.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-mini-project-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,MatCardModule],
   templateUrl: './mini-project-card.component.html',
   styleUrls: ['./mini-project-card.component.scss']
 })
 export class MiniProjectCardComponent {
-
 }
