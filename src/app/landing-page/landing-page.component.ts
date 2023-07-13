54155c754a834d5c1767e48add4e7c73e8fb721f
mohammed <mohammed.elbakkalielammari@etu.uae.ac.ma>
2023-07-13 17:07:08 +0100

    add category component

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { CategoryComponent } from "../category/category.component";
 import { CardComponent } from "../card/card.component";
 
 import {CdkDragDrop, moveItemInArray, CdkDrag, CdkDropList} from '@angular/cdk/drag-drop';
 import {NgFor} from '@angular/common';
 import {MatChipsModule} from '@angular/material/chips';
 import {MatMenuModule} from '@angular/material/menu';
+import { CategoryOfmodelComponent } from '../category-ofmodel/category-ofmodel.component';
 
 
 export interface Vegetable {
@@ -18,7 +18,7 @@ export interface Vegetable {
     styleUrls: ['./landing-page.component.scss'],
     standalone: true,
 
-    imports: [CategoryComponent,MatChipsModule, CdkDropList, NgFor, CdkDrag,MatMenuModule]
+    imports: [MatChipsModule, CdkDropList, NgFor, CdkDrag,MatMenuModule,CategoryOfmodelComponent]
 
 })
 export class LandingPageComponent {
