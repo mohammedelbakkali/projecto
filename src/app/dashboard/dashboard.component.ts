1602c28151884a708b437f027d31cf25267990f5
chihab <hellserc2@gmail.com>
2023-07-17 09:53:31 +0100

    trial

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SideBarComponent } from '../shared/side-bar/side-bar.component';
 import { CardComponent } from "../card/card.component";
@@ -12,22 +12,34 @@ import { MatButtonModule } from '@angular/material/button';
     standalone: true,
     imports: [CommonModule, CardComponent, MiniProjectCardComponent,MatIconModule,MatButtonModule,SideBarComponent]
 })
-export class DashboardComponent implements AfterViewInit{
-    constructor(private elementRef: ElementRef) {}
-
-  /**   @ViewChild('sideBarRef', { read: ElementRef }) sideBarRef: ElementRef | undefined;
-
+export class DashboardComponent{
+sidebarVisible = false;
+ /**    constructor(private elementRef: ElementRef, private renderer: Renderer2) {
+        this.modifyMargin();
+    }
+    modifyMargin() {
+        const element: HTMLElement = this.elementRef.nativeElement;
+        this.renderer.setStyle(element, 'margin-left', '250px');
+      }
+      @ViewChild('sideBarRef', { read: ElementRef }) sideBarRef: ElementRef | undefined;
+width: any;
     ngAfterViewInit() {
       const sideBarElement: HTMLElement = this.sideBarRef!.nativeElement;
-      const width: number = sideBarElement.offsetWidth;
+    const width: number = sideBarElement.offsetWidth;
       console.log('Child component width:', width);
+
+      const resizeObserver = new ResizeObserver(entries => {
+        for (const entry of entries) {
+          if (entry.target === sideBarElement) {
+            this.width = entry.contentRect.width;
+            console.log('Child component width:', this.width);
+          }
+        }
+      });
+  
+      resizeObserver.observe(sideBarElement);
     }
    
 */
-ngAfterViewInit() {
-    const childElement: HTMLElement = this.elementRef.nativeElement.querySelector('#sideBarRef');
-    const width: number = childElement.offsetWidth;
-    console.log('Child component width:', width);
-  }
 
 }
--- src/app/dashboard/mini-project-card/mini-project-card.component.ts
+++ src/app/dashboard/mini-project-card/mini-project-card.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
-
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
 @Component({
   selector: 'app-mini-project-card',
   standalone: true,
-  imports: [CommonModule,MatCardModule],
+  imports: [CommonModule,MatCardModule,MatMenuModule,MatIconModule],
   templateUrl: './mini-project-card.component.html',
   styleUrls: ['./mini-project-card.component.scss']
 })
