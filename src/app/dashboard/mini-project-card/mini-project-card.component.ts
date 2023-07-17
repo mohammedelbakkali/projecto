bda91df64c6ed31245f8d258460a3f5984513088
chihab <hellserc2@gmail.com>
2023-07-17 11:07:33 +0100

    dashboard semi complete

--- src/app/dashboard/mini-project-card/mini-project-card.component.ts
+++ src/app/dashboard/mini-project-card/mini-project-card.component.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-mini-project-card',
   standalone: true,
-  imports: [CommonModule,MatCardModule,MatMenuModule,MatIconModule],
+  imports: [CommonModule,MatCardModule,MatMenuModule,MatIconModule,MatButtonModule],
   templateUrl: './mini-project-card.component.html',
   styleUrls: ['./mini-project-card.component.scss']
 })
