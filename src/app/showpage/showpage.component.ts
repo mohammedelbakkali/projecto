cae29a4262ea835d08e86ab3271958cb8d0221a2
ZAKARIAE48CHELLE <118846093+ZAKARIAE48CHELLE@users.noreply.github.com>
2023-07-18 09:49:51 +0100

    workung on ratt

--- src/app/showpage/showpage.component.ts
+++ src/app/showpage/showpage.component.ts
@@ -22,6 +22,7 @@ export interface Projecto{
   prjt_hdrs: string [];
   description: string;
   ppl_targeted: string [] ;
+  rating : string;
 }
 
 
@@ -102,10 +103,17 @@ export class ShowpageComponent {
 
   proj !: Projecto ;
 
+// ==   RATING ==
 
+selectedRating: string ='';
 
-
-
+updateRating(target: EventTarget | null): string {
+  if (target instanceof HTMLInputElement) {
+    this.selectedRating = target.value;
+    console.log(this.selectedRating)
+  }
+  return this.selectedRating;
+}
 
 
 
@@ -145,7 +153,9 @@ export class ShowpageComponent {
 
   description :"If you're an office worker, student, administrator, or just want to become more productive with your computer, programming will allow you write code that can automate tedious tasks. This course follows the popular (and free!) book, Automate the Boring Stuff with Python. Automate the Boring Stuff with Python was written for people who want to get up to speed writing small programs that do practical tasks as soon as possible. You don't need to know sorting algorithms or object-oriented programming, so this course skips all the computer science and concentrates on writing code that gets stuff done. This course is for complete beginners and covers the popular Python programming language. You'll learn basic concepts as well as:Web scrapingParsing PDFs and Excel spreadsheetsAutomating the keyboard and mouse Sending emails and texts And several other practical topics By the end of this course, you'll be able to write code that not only dramatically increases your productivity, but also be able to list this fun and creative skill on your resume.",
 
-  ppl_targeted :['Office workers, students, small/home business workers, and administrators would want to improve their productivity.','Aspiring software engineers who want to add skills to their programming toolbelt.','Computer users who have heard the "learn to code" message, but want practical reasons to learn programming.','Experienced Python software engineers can skip the first half of the course, but may find the later parts that cover various third-party modules helpful.',"While this course doesn't cover specific devops tools, this course would be useful for QA, devops, and admins who want to learn scripting in Python"]
+  ppl_targeted :['Office workers, students, small/home business workers, and administrators would want to improve their productivity.','Aspiring software engineers who want to add skills to their programming toolbelt.','Computer users who have heard the "learn to code" message, but want practical reasons to learn programming.','Experienced Python software engineers can skip the first half of the course, but may find the later parts that cover various third-party modules helpful.',"While this course doesn't cover specific devops tools, this course would be useful for QA, devops, and admins who want to learn scripting in Python"],
+
+  rating : this.selectedRating
 
     }
   }
