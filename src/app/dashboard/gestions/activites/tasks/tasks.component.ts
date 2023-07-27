69351113b94d7483557d71a37f300ce47f020dd7
mohammed <mohammed.elbakkalielammari@etu.uae.ac.ma>
2023-07-27 23:29:31 +0100

    add tasks

--- src/app/dashboard/gestions/activites/tasks/tasks.component.ts
+++ src/app/dashboard/gestions/activites/tasks/tasks.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
+import { ListTaskComponent } from "./list-task/list-task.component";
 
 @Component({
-  selector: 'app-tasks',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.scss']
+    selector: 'app-tasks',
+    standalone: true,
+    templateUrl: './tasks.component.html',
+    styleUrls: ['./tasks.component.scss'],
+    imports: [CommonModule, MatTabsModule, ListTaskComponent]
 })
 export class TasksComponent {
 
